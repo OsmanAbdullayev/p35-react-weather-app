185d81d1321ad3ecc5b675a35fea83b9088fd75d
Osman Abdullayev <os.abdullayev@gmail.com>
2022-11-09 15:48:53 +0400

    after changin the favicon and  correcting styles

--- src/App.js
+++ src/App.js
@@ -78,7 +78,7 @@ function App() {
 							<div className="date">{dateBuilder(new Date())}</div>
 						</div>
 						<div className="weather-box">
-							<div className="temp">{Math.round(weather.main.temp)} ℃</div>
+							<div className="temp">{Math.round(weather.main.temp)} °C</div>
 							<div className="weather">{weather.weather[0].main}</div>
 						</div>
 					</section>
